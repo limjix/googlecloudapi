63fc0a85fd07ca236c59b169ec2e4131615ec121
limjix <limjix@gmail.com>
2020-02-17 22:14:11 +0800

    no message

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
 from model import app
+from flask import Flask,jsonify, request
+from flask_jwt_extended import (
+    JWTManager, jwt_required, create_access_token,
+    get_jwt_identity
+)
 
 @app.route("/")
 def hello():
